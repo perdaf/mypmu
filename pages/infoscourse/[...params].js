fd2685ba76777e28830b63716bdb503a1a41b2a1
Perdaf Steeve <perdafsteeve@free.fr>
2022-05-13 11:48:31 -0400

    Update [...params].js

--- pages/infoscourse/[...params].js
+++ pages/infoscourse/[...params].js
@@ -2,20 +2,22 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 // import styles from "../../styles/Home.module.css";
 
-export default function coursesdujour() {
-  const router = useRouter();
-  const { params } = router.query;
-
-  console.log(params);
-
+export default function coursesdujour(result) {
+  console.log(result.res.participants);
   return <h1>test infos course</h1>;
 }
 
 export const getServerSideProps = async ({ params }) => {
-  /* const res = await fetch(
-    `https://online.turfinfo.api.pmu.fr/rest/client/62/programme/${date}/R1/C1/participants?specialisation=OFFLINE`
-  ).then((data) => data.json());*/
+  const date = Object.values(params)[0][0];
+  const reunion = Object.values(params)[0][1];
+  const course = Object.values(params)[0][2];
+
+  const res = await fetch(
+    `https://online.turfinfo.api.pmu.fr/rest/client/62/programme/${date}/R${reunion}/C${course}/participants?specialisation=OFFLINE`
+  ).then((data) => data.json());
   return {
-    props: {},
+    props: {
+      res,
+    },
   };
 };
